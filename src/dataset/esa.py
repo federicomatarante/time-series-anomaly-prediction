45296f65a04450b3560d4fbf4d0f50531addc2c3
Odysseas Diamadis <odysseas@dilmun.net>
2025-01-16 17:18:33 +0100

    fixed dataset

--- src/dataset/esa.py
+++ src/dataset/esa.py
@@ -63,16 +63,16 @@ class ESADataset(Dataset):
         self.start_time = first_index
     
     def __len__(self):
-        return self.length
+        return len(self.dataset.index)
     
     def __getitem__(self, idx):
-        start_index = self.start_time + self.delta_index*idx*self.stride
+        start_index = self.start_time + self.delta_index*idx
         end_index = start_index + self.delta_index*self.window_size
         start_horizon_index = end_index + self.delta_index
         end_horizon_index = start_horizon_index + self.delta_index*self.horizon_size
         labels = np.array([
             1.0 if any([ True if i == 1.0 else False for i in x ]) else 0.0
-            for x in self.anomalies.loc[start_horizon_index:end_horizon_index].to_numpy()
+            for x in self.anomalies.loc[start_horizon_index:end_horizon_index].to_numpy().transpose(1,0)
         ])
         
         return {
