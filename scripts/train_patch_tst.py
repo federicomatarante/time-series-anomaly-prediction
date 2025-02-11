4b2b38a3a747404d5069ef60a6a459d72863f227
Odysseas Diamadis <odysseas@dilmun.net>
2025-02-11 16:20:52 +0100

    ckpt name from argv[1]

--- scripts/train_patch_tst.py
+++ scripts/train_patch_tst.py
@@ -1,12 +1,14 @@
 from scripts.utils import train_model
 from src.trainings.base_patchtst_trainer import BasePatchTSTTrainer
+import sys
 
 if __name__ == '__main__':
+    ckpt_name = sys.argv[1]
     train_model(
         dataset_config_name='esa.ini',
         model_config_name='patchtst.ini',
         train_config_name='training.ini',
         logs_path='logs/patch_tst_training',
         trainer_class=BasePatchTSTTrainer,
-        checkpoint_file_name='last'
+        checkpoint_file_name=ckpt_name,
     )
